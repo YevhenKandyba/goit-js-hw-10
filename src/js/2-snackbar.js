66b122f9e952ecbc9fa2c746494e5e0360f3fc40
YevhenKandyba <kandyba0311@gmail.com>
2024-11-10 01:46:53 +0100

    Add Home Work 10

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,37 @@
-console.log('Snackbar');
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const delay = parseInt(form.delay.value);
+    const state = form.state.value;
+
+    function createPromise(delay, state) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (state === 'fulfilled') {
+            resolve(delay);
+          } else {
+            reject(delay);
+          }
+        }, delay);
+      });
+    }
+
+    createPromise(delay, state)
+      .then((delay) => {
+        iziToast.success({
+          message: `✅ Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch((delay) => {
+        iziToast.error({
+          message: `❌ Rejected promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
+  });
+});
+
